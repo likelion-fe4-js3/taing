72b97efb5ad99c3dbff230b9fd565555683e66df
hardy-is-cat <whwjdguszz@naver.com>
2023-02-02 17:27:18 +0900

    [feat] 메인페이지 swiper 생성(모바일) close #27

--- client/js/main.js
+++ client/js/main.js
@@ -26,3 +26,45 @@ const quickVod = new Swiper(".quick-vod.swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const realtimePopular = new Swiper(
+  ".realtime-popular.swiper",
+  {
+    slidesPerView: "auto",
+    spaceBetween: 8,
+    keyboard: {
+      enabled: true,
+    },
+    loop: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  }
+);
+
+const livePopular = new Swiper(".live-popular.swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 8,
+  keyboard: {
+    enabled: true,
+  },
+  loop: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const onlyTaing = new Swiper(".only-taing.swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 8,
+  keyboard: {
+    enabled: true,
+  },
+  loop: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
