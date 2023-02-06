fa752384432bc0d7f66ca361afb95b98309ab303
Byeonjin <quswlstkd88@naver.com>
2023-02-06 21:39:23 +0900

    [feature] landingpage rolling banner 반응형 대응 #61 #64

--- client/js/landing.js
+++ client/js/landing.js
@@ -2,39 +2,6 @@ import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.brow
 
 import { getNode, setCss } from "../lib/index.js";
 
-function getContentX(node) {
-  node = getNode(node);
-  return (
-    window.pageYOffset + node.getBoundingClientRect().top
-  );
-}
-
-function getContentHeight(node) {
-  node = getNode(node);
-  console.log(node.getBoundingClientRect.height);
-}
-
-document.addEventListener("scroll", function () {
-  if (
-    document.documentElement.scrollTop >=
-      getContentX(".scroll-section-1") &&
-    document.documentElement.scrollTop <
-      getContentX(".scroll-section-2") - window.innerHeight
-  ) {
-    //setCss(".scroll-section-1", "position", "sticky");
-    //setCss(".scroll-section-1", "top", "3rem");
-  }
-
-  // else if (
-  //   document.documentElement.scrollTop >
-  //   getContentX(".scroll-section-2") - window.innerHeight
-  // ) {
-  //   setCss(".scroll-section-1", "position", "static");
-  // }
-
-  //console.log(getContentHeight(".scroll-section-1"));
-});
-
 const swiper = new Swiper(".upper-wrapper", {
   loop: true,
   loopedSlides: 1,
@@ -43,13 +10,21 @@ const swiper = new Swiper(".upper-wrapper", {
     disableOnInteraction: false,
   },
   spaceBetween: 10,
-  width: 900,
-  slidesPerView: 5,
   speed: 3000,
   grabCursor: false,
   mousewheelControl: false,
   keyboardControl: true,
   allowTouchMove: false,
+  breakpoints: {
+    320: {
+      width: 900,
+      slidesPerView: 5,
+    },
+    1200: {
+      width: 1400,
+      slidesPerView: 3,
+    },
+  },
 });
 
 const lower_swiper = new Swiper(".lower-wrapper", {
@@ -61,11 +36,19 @@ const lower_swiper = new Swiper(".lower-wrapper", {
     disableOnInteraction: false,
   },
   spaceBetween: 10,
-  width: 900,
-  slidesPerView: 5,
   speed: 2000,
   grabCursor: false,
   mousewheelControl: false,
   keyboardControl: true,
   allowTouchMove: false,
+  breakpoints: {
+    320: {
+      width: 900,
+      slidesPerView: 5,
+    },
+    1200: {
+      width: 1400,
+      slidesPerView: 3,
+    },
+  },
 });
