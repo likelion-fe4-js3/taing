b54f7c3926ac07dd932b518551b6d7ca34f0e444
hardy-is-cat <whwjdguszz@naver.com>
2023-02-07 01:04:38 +0900

    [chore] 메인페이지 메인비주얼 병합

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,46 @@
+import { attr, getNode } from "../lib/index.js";
+
+// 헤더 스크롤 액션
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    header.classList.remove("deactive");
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    header.classList.add("deactive");
+  }
+});
+
+// main-visual swiper
+const mainVisual = new Swiper(".main-visual", {
+  loop: true,
+  speed: 1500,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 2000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// main-visual autoplay, pause
+const autoplayBtn = getNode(".swiper-button-autoplay");
+
+function handlerAutoplay() {
+  console.log(attr(autoplayBtn, "class"));
+  mainVisual.autoplay.stop();
+}
+
+autoplayBtn.addEventListener("click", handlerAutoplay);
+
 const recommendTaing = new Swiper(
   ".recommend-taing .swiper",
   {
@@ -155,5 +198,18 @@ const onlyTaing = new Swiper(".only-taing .swiper", {
       spaceBetween: 16,
       slidesPerView: 7,
     },
+  },
+});
+
+const eventSwiper = new Swiper(".event", {
+  slidesPerView: "auto",
+  spaceBetween: 8,
+  keyboard: {
+    enabled: true,
+  },
+  loop: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
